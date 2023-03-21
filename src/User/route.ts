fb6d88af407fa842be252a1193d9b15b07471e6d
maguitaria <t2glma00@students.oamk.fi>
2023-03-21 15:39:56 +0200

    changed the structure

---
+++ src/User/route.ts
@@ -0,0 +1,13 @@
+import { RouteConfig } from "../Common/config/route"
+import express, { Application, Request, Response } from "express"
+import UserController from "./controller"
+
+export class UserRoutes extends RouteConfig {
+    constructor(app: Application) {
+        super(app, "UserRoutes")
+    }
+    configureRoutes() {
+        this.app.route(`/users`).get([UserController.getUsers])
+        return this.app
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,45 @@
 import express from 'express';
+import cors from 'cors';
+import * as dotenv from "dotenv";
+import mongoose from 'mongoose';
+const http = require("http").createServer(express)
+const io = require("socket.io")(http)
 
-const app = express();
+const app: Express = express();
+const PORT: any = process.env || 8081;
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@atlascluster.rtquvey.mongodb.net/${process.env.MONGO_DB}`
 
+
+mongoose
+    .connect(uri)
+    .then(() =>
+        app.listen(PORT, () =>
+            console.log(`Server running on http://localhost:${PORT}`)
+        )
+    )
+    .catch((error) => {
+        throw error
+    })
+
+dotenv.config()
+
+
+mongoose
+    .connect(uri)
+    .then(() =>
+        app.listen(PORT, () =>
+            console.log(`Server running on http://localhost:${PORT}`)
+        )
+    )
+    .catch((error) => {
+        throw error
+    })
+app.use(cors())
+app.use(express.json())
+app.use("/api/user", userRouter);
+app.use("/api/post", PostRouter)
 // declare a route with a response
 app.get('/', (req, res) => {
     res.send("What's up doc ?!");
 });
 
-// start the server
-app.listen(8081, () => {
-    console.log(`server running : http://localhost:8081`);
-});
