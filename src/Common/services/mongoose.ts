160917c2b0d9db56b2bfdfd89c4f8ae2167a8960
maguitaria <t2glma00@students.oamk.fi>
2023-03-23 18:00:14 +0200

    added mongoose

--- src/Common/services/mongoose.ts
+++ src/Common/services/mongoose.ts
@@ -1,8 +1,20 @@
 import mongoose from "mongoose";
 import debug, { IDebugger } from "debug" 
 
+
 const log: IDebugger = debug("app:mongoose-service")
 
+// // Create an anonymous credential
+// const credentials = Realm.Credentials.anonymous();
+// try {
+//     const user = await app.logIn(credentials);
+//     console.log("Successfully logged in!", user.id);
+//     return user;
+// } catch (err) {
+//     if (err instanceof Error) {
+//         console.error("Failed to log in", err.message);
+//     }
+// }
 
 class MongooseService {
     private count = 0
