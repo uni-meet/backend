d90b77bf995db4a157064fb20c267569eb80d959
maguitaria <t2glma00@students.oamk.fi>
2023-03-31 14:22:22 +0300

    add readme

--- README.md
+++ README.md
@@ -31,6 +31,34 @@ This backend is created with:
 
 ### Pictures
 
-## Useful links
+- Share Picture
+- Get Picture (single)
+- Get Pictures By User
+- Delete Pictures
+- Update Caption
 
-p.s. API documentation is coming. The endpoints can be found via `src/routes/router.ts`
+### API endpoints
+
+All the endpoints follow the structure:
+https://localhost:PORT/api/ENDPOINTS
+For the frontend usage, documentation should be added here:
+
+- Space for link to Readme.md API documentation
+
+## For Backend Devs
+
+You can find all endpoints system via `./src/routes/router.ts`
+All controller functions are at path `./src/controllers/...`
+There are two models in project:
+
+- picture model (database test_pictures)
+- user model (database user_pictures)
+
+Tasks:
+
+- :U+1F525: create an endpoint to get All users info from db (for main page)
+- test endpoints with REST API
+- create documentation for API endpoints for frontend devs
+- host backend,database, API
+
+## Useful links
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -17,8 +17,18 @@ router.get('/test', (req: Request, res: Response): void => {
     debuglog('LOG', 'router - test', 'Router test success')
     res.json({ 'result': ' router test success' })
 })
-// users
-// Herema
+
+// Here the HTTP methods may be changed. When doing that, check that functions of changed HTTP requests
+// have the needed requirements ( the line req.body... should be changed  to req.params... in functions with GET request)
+// Also, follow the right path for the API.
+/**
+ * When it is GET method 
+ * For ex: router.get('/user/nameOfFunction/:id', next function) 
+ * When it is POST method, no values are added to URL 
+ * For ex: router.post('/signup', userCtrl.signup)
+ */
+
+// users API endpoints
 router.post('/signup', userCtrl.signup)
 router.post('/login', userCtrl.login)
 router.get('/user/getInfo/:id', userCtrl.getUserInfo)
@@ -27,3 +37,5 @@ router.put('/user/updateUserInfo', userCtrl.updateUserInfo)
 router.patch('/user/updateUserPassword', userCtrl.updateUserPassword)
 router.delete('/user/deleteUser', userCtrl.deleteUser)
 export { router }
+
+// pictures API endpoints 
