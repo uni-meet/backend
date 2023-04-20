27353325ae35b2841bd8e3e817c206d64a254be2
maguitaria <t2glma00@students.oamk.fi>
2023-04-20 18:19:53 +0300

    added error handling

--- src/controllers/search_controller.ts
+++ src/controllers/search_controller.ts
@@ -6,6 +6,7 @@ import mongoose from 'mongoose';
 import { debuglog } from '../helpers';
 import { Picture, User } from "../models";
 import { Request, Response } from "express";
+import { error } from 'console';
 
 // /** Searches given a searchString
 //  * @param {string} req.body.searchString Given search string
@@ -33,49 +34,50 @@ import { Request, Response } from "express";
 //FIXME - not working
 export async function searchUser(req: Request, res: Response) {
 
-        if (!req.body.searchString) {
-            res.status(400).json({ result: 'error', message: 'unsatisfied requirements' });
-            return;
-        }
-        User.aggregate([
-            {
-                $match: {
-                    $text: { $search: req.body.searchString }
-                }
-            },
-            {
-                $lookup: {
-                    from: "pictures",
-                    localField: "_id",
-                    foreignField: "userId",
-                    as: "pictures"
-                }
-            },
-            {
-                $project: {
+    if (!req.body.searchString) {
+        res.status(400).json({ result: 'error', message: 'unsatisfied requirements' });
+        return;
+    }
+    User.aggregate([
+        {
+            $match: {
+                $text: { $search: req.body.searchString }
+            }
+        },
+        {
+            $lookup: {
+                from: "pictures",
+                localField: "_id",
+                foreignField: "userId",
+                as: "pictures"
+            }
+        },
+        {
+            $project: {
+                _id: 1,
+                username: 1,
+                firstName: 1,
+                lastName: 1,
+                pictures: {
                     _id: 1,
-                    username: 1,
-                    firstName: 1,
-                    lastName: 1,
-                    pictures: {
-                        _id: 1,
-                        userId: 1,
-                        pictureImage: 1,
-                        description: 1,
-                        comments: 1,
-                        likes: 1
-                    }
+                    userId: 1,
+                    pictureImage: 1,
+                    description: 1,
+                    comments: 1,
+                    likes: 1
                 }
             }
-        ])
-        
-            .then(searchResults => {
-                debuglog('LOG', 'search controller - search users', 'completed search');
-                res.status(200).json({ result: 'success', data: searchResults });
-            })
-            .catch(error => {
-                debuglog('ERROR', 'search controller - search users', error);
-                res.status(400).json(error);
-            });
-    }
+        }
+    ])
 
+        .then(searchResults => {
+            debuglog('LOG', 'search controller - search users', 'completed search');
+            res.status(200).json({ result: 'success', data: searchResults });
+        })
+
+        .catch(error); {
+        console.log(error)
+        res.status(500).send('Error searching results')
+
+    }
+}
