df8bd95dd8a5324f39e6f81dd2dbf224068dba38
Maksim Kantur <maxim.kantur@hotmail.com>
2023-05-03 00:57:16 +0300

    test 3

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,9 @@ import { connectDB } from './config';
 import { debuglog } from './helpers/debuglog';
 import { router } from './routes';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import Grid from 'gridfs-stream';
+import { GridFSBucketReadStream } from 'mongodb';
 
 const app: Application = express();
 
@@ -35,6 +38,45 @@ app.listen(PORT, (): void => { // TODO: Study how to pass host
     debuglog('LOG', 'server', `Server is listening on port ${PORT}`)
     console.log('Server working on http://localhost:8082')
 })
+let gfs: Grid.Grid;
+const conn: mongoose.Connection = mongoose.connection;
+conn.once('open', () => {
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection('uploads');
+});
+
+interface GridFSFile {
+    _id: any;
+    length: number;
+    chunkSize: number;
+    uploadDate: Date;
+    md5: string;
+    filename: string;
+    contentType: string;
+    aliases: any;
+    metadata: any;
+  }
+
+// Add a route to serve images
+app.get('/uploads/:filename', (req, res) => {
+    (gfs.files as any).findOne({ filename: req.params.filename }, (err: Error | null, file: GridFSFile | null) => {
+      if (!file || file.length === 0) {
+        return res.status(404).json({
+          err: 'No file exists',
+        });
+      }
+  
+      if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+        // Read the image and pipe it to the response
+        const readstream = gfs.createReadStream(file.filename);
+        readstream.pipe(res);
+      } else {
+        res.status(404).json({
+          err: 'Not an image',
+        });
+      }
+    });
+  });
 
 
 
