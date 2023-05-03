1c7dcd82b3c84717086852da33cff3eb7e751b41
Maksim Kantur <maxim.kantur@hotmail.com>
2023-05-03 11:31:43 +0300

    test5

--- src/config/images.ts
+++ src/config/images.ts
@@ -10,23 +10,26 @@ import MulterGridfsStorage from "multer-gridfs-storage";
 
 // import env variables
 
-
+import mongoose from "mongoose";
 import multer from 'multer';
 import { Request } from 'express';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const GridFsStorage = require('multer-gridfs-storage');
 
-const MONGODB_URI = process.env.MONGODB_URI;
+
+const MONGODB_URI = "mongodb+srv://adminAll:adminAll@cluster0.mongodb.net/newDB?retryWrites=true&w=majority";
+const conn = mongoose.createConnection(MONGODB_URI);
 
 const storage = new GridFsStorage({
-  url: MONGODB_URI,
-  file: (req: Request, file: Express.Multer.File) => {
-    return {
-      filename: Date.now() + '-' + file.originalname,
-    };
-  },
-});
+    url: MONGODB_URI,
+    file: (req: Request, file: Express.Multer.File) => {
+      return {
+        bucketName: "uploads",
+        filename: `${Date.now()}-${file.originalname}`,
+      };
+    },
+  });
 
 const upload = multer({ storage });
 
